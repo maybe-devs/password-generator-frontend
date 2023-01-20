91701fd5613d07dca885b974ac0fb7906d84d704
Mornieur <mariafernanda-coro@hotmail.com>
2023-01-20 00:41:57 -0300

    feat: slider component
    generated number size slider

---
+++ src/components/homeComponent/Header/InputSlider/index.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import MuiInput from '@mui/material/Input';
+// import VolumeUp from '@mui/icons-material/VolumeUp';
+
+const Input = styled(MuiInput)`
+  width: 70px;
+  padding: 0.5rem;
+  background-color: #fff;
+  border-radius: 5px;
+  display: flex;
+  box-shadow: #e3e3e3;
+`;
+
+export default function InputSlider() {
+  const [value, setValue] = React.useState<
+    number | string | Array<number | string>
+  >(30);
+
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 100) {
+      setValue(100);
+    }
+  };
+
+  return (
+    <Box sx={{ width: 250 }}>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item>
+          <Input
+            value={value}
+            size="small"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 10,
+              min: 0,
+              max: 100,
+              type: 'number',
+              'aria-labelledby': 'input-slider',
+            }}
+          />
+        </Grid>
+        <Grid item xs>
+          <Slider
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
