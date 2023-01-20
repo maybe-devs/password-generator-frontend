7f50f917371598446ad1c04cf18fba1caffa8138
Mornieur <mariafernanda-coro@hotmail.com>
2023-01-20 00:37:48 -0300

    style: home component styles
    placing components in the middle

--- src/components/homeComponent/styles.ts
+++ src/components/homeComponent/styles.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: rgb(249, 181, 210);
+  background: radial-gradient(
+    circle,
+    rgba(249, 181, 210, 1) 0%,
+    rgba(148, 187, 233, 1) 100%
+  );
+`;
