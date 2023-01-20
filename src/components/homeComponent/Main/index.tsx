b54b3428e8633a1a7cad0dcdf17795196412fe0a
Mornieur <mariafernanda-coro@hotmail.com>
2023-01-20 00:44:57 -0300

    feat(main component): password view
    component that renders the new password and contains buttons

---
+++ src/components/homeComponent/Main/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles';
+import { MdOutlineContentCopy } from 'react-icons/md';
+import { TfiReload } from 'react-icons/tfi';
+
+export const Main = () => {
+  return (
+    <S.Container>
+      <p>gFJ#FjOp521F</p>
+      <div>
+        <MdOutlineContentCopy className="icon" />
+        <TfiReload className="icon" />
+      </div>
+    </S.Container>
+  );
+};
