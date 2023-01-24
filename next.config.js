b24519466414756603a3d38868b4521ec9fb4fad
Mornieur <mariafernanda-coro@hotmail.com>
2023-01-24 12:17:33 -0300

    chore(next config): next configuration
    setting base url in nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    PASSWORD_URL: process.env.PASSWORD_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
