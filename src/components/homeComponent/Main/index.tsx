ea0c877ed3ddfc6a79e9b0e82860fa281458c270
Mornieur <mariafernanda-coro@hotmail.com>
2023-01-24 12:30:03 -0300

    feat(reload password): add post into reload button
    adding change passwords to button next to context

--- src/components/homeComponent/Main/index.tsx
+++ src/components/homeComponent/Main/index.tsx
@@ -1,14 +1,23 @@
 import * as S from './styles';
 import { MdOutlineContentCopy } from 'react-icons/md';
 import { TfiReload } from 'react-icons/tfi';
+import { usePostPassword } from '@/global/services/POST/usePostPassword';
+import { useContext } from 'react';
+import { PasswordDetailsContext } from '@/global/context';
 
 export const Main = () => {
+  const { postPassword, isSuccessPostPassword } = usePostPassword();
+
+  const { passwordInfo, setPasswordInfo } = useContext(PasswordDetailsContext);
+  console.log(passwordInfo);
   return (
     <S.Container>
       <p>gFJ#FjOp521F</p>
       <div>
         <MdOutlineContentCopy className="icon" />
-        <TfiReload className="icon" />
+        <button onClick={() => postPassword(passwordInfo)}>
+          <TfiReload className="icon" />
+        </button>
       </div>
     </S.Container>
   );
