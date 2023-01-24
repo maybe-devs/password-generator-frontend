e2daa287db42cb7445bc8605cc25a23a3bf9d702
Mornieur <mariafernanda-coro@hotmail.com>
2023-01-24 12:24:26 -0300

    feat(context): add context details
    adding context to main file

--- src/components/homeComponent/index.tsx
+++ src/components/homeComponent/index.tsx
@@ -4,13 +4,16 @@ import { Header } from './Header';
 import { Title } from './Title';
 
 import * as S from './styles';
+import { PasswordDetailsProvider } from '@/global/context';
 
 export const HomeComponent = () => {
   return (
     <S.Container>
-      <Title />
-      <Main />
-      <Header />
+      <PasswordDetailsProvider>
+        <Title />
+        <Main />
+        <Header />
+      </PasswordDetailsProvider>
     </S.Container>
   );
 };
